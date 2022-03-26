f32c385995a7ce9c30ca7669ef005b1d9b0c3b3c
Thamjith Thaha <jithuthaha8@gmail.com>
2022-03-26 19:17:53 +0530

    Completed Project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA-H284wRzQ6f1ThJgMZNbUeqPCnEenXPg',
+    authDomain: 'clips-1a05f.firebaseapp.com',
+    projectId: 'clips-1a05f',
+    storageBucket: 'clips-1a05f.appspot.com',
+    messagingSenderId: '807669160123',
+    appId: '1:807669160123:web:9a86f16ea1ba481eb9b19e',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA-H284wRzQ6f1ThJgMZNbUeqPCnEenXPg',
+    authDomain: 'clips-1a05f.firebaseapp.com',
+    projectId: 'clips-1a05f',
+    storageBucket: 'clips-1a05f.appspot.com',
+    messagingSenderId: '807669160123',
+    appId: '1:807669160123:web:9a86f16ea1ba481eb9b19e',
+  },
 };
 
 /*
